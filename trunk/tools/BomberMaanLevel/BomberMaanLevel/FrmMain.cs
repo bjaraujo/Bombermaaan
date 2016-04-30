c81f3998138bc909eeda7a7b563ccf332e86500b
billyaraujo <billy@multiseis.com>
2016-04-30 08:55:08 +0100

    started bombermaan level editor.

---
+++ trunk/tools/BomberMaanLevel/BomberMaanLevel/FrmMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BomberMaanLevel
+{
+    public partial class FrmMain : Form
+    {
+
+        private LevelManager levelManager = new LevelManager();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+            this.Text = "BomberMaan Level Editor";
+
+            this.Width = levelManager.NumberOfTilesX() * 64;
+            this.Height = levelManager.NumberOfTilesY() * 64;
+
+        }
+    }
+}
---
+++ trunk/tools/BomberMaanLevel/BomberMaanLevel/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberMaanLevel
+{
+
+    class LevelManager
+    {
+        const int nbTilesX = 16;
+        const int nbTilesY = 12;
+
+        public int NumberOfTilesX()
+        {
+            return nbTilesX;
+        }
+
+        public int NumberOfTilesY()
+        {
+            return nbTilesY;
+        }
+
+    }
+}
