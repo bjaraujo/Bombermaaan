f9846fc8c759f9bd442fb122b6fc9149684eac0c
bjaraujo <billyaraujo@gmail.com>
2017-12-30 13:17:17 +0000

    Adding cross-compile files for raspberry-pi.

---
+++ cross-compile-env.sh
@@ -0,0 +1,9 @@
+export ARCH=arm
+export CROSS_COMPILE=arm-bcm2708hardfp-linux-gnueabi-
+export CC=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc
+export NM=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-nm
+export LD=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ld
+export CXX=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++
+export RANLIB=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ranlib
+export AR=${PI_TOOLS_HOME}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ar
+
