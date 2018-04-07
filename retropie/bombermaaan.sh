627d0ff40dd7e539218f7366cbe840e2fbbf8625
bjaraujo <billyaraujo@gmail.com>
2018-04-07 13:29:25 +0100

    Building for linux

--- retropie/bombermaaan.sh
+++ retropie/bombermaaan.sh
@@ -24,13 +24,15 @@ function sources_bombermaaan() {
 }
 
 function build_bombermaaan() {
+    cd trunk
     cmake . -DCMAKE_INSTALL_PREFIX="$md_inst" -DLOAD_RESOURCES_FROM_FILES:BOOL=ON
     make
     md_ret_require="$md_build/trunk/bombermaaan"
 }
 
 function install_bombermaaan() {
-    md_ret_files=(
+    md_ret_files=(        
+        'bombermaaan'
         'trunk/levels'
         'trunk/res/images'
         'trunk/res/sounds'
