7639c0bd0335327f99148ac7555a550a469113a8
Billy Araujo <billyaraujo@gmail.com>
2020-12-04 17:00:25 +0000

    Updated openai env.

--- openai/gym-bombermaaan/gym_bombermaaan/envs/bombermaaan_env.py
+++ openai/gym-bombermaaan/gym_bombermaaan/envs/bombermaaan_env.py
@@ -1,50 +1,59 @@
+import win32process
+import win32gui
+import win32api
+import win32con
+import time
 import gym
 from gym import spaces
+from PIL import Image, ImageGrab 
 
 import logging
 logger = logging.getLogger(__name__)
 
 class BombermaaanEnv(gym.Env):
-    metadata = {'render.modes': ['human', 'rgb_array']}
 
     def __init__(self):
 
-        screen_height = 442
-        screen_width = 480
-    
-        self.observation_space = spaces.Box(low=0, high=255, shape=(screen_height, screen_width, 3))
+        startObj = win32process.STARTUPINFO()
+        win32process.CreateProcess('C:\\Program Files (x86)\\Bombermaaan\\Bombermaaan.exe', 'Bombermaaan.exe --use-appdata-dir', None, None, 8, 8, None, 'C:\\Program Files (x86)\\Bombermaaan', startObj)
+
+        time.sleep(2)
+
+        x0 = 0
+        y0 = 0
+        x1 = 0
+        y1 = 0
+
+        self.whnd = win32gui.FindWindowEx(None, None, None, 'Bombermaaan 2.0.8.2182')
+        if not (self.whnd == 0):
+            x0, y0, x1, y1 = win32gui.GetWindowRect(self.whnd)
+
+        self.x0 = x0 + 8
+        self.y0 = y0 + 1
+        self.x1 = x1 - 8
+        self.y1 = y1 - 8
+
+        self.width = self.x1 - self.x0
+        self.height = self.y1 - self.y0
         
-    def _step(self, action):
-        reward = 0.0
-        action = self._action_set[a]
-
-        if isinstance(self.frameskip, int):
-            num_steps = self.frameskip
-        else:
-            num_steps = self.np_random.randint(self.frameskip[0], self.frameskip[1])
-        for _ in range(num_steps):
-            reward += self.ale.act(action)
-        ob = self._get_obs()
-
-        return ob, reward, self.ale.game_over(), {"ale.lives": self.ale.lives()}
+        self.action_space = spaces.Discrete(6)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(self.height, self.width, 3))
         
     def _reset(self):
-        self.ale.reset_game()
+        self.t = 0
+        win32api.PostMessage(self.whnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
         return self._get_obs()
         
+    def _step(self, action):
+        self.t = self.t + 1
+        ob = self._get_obs()
+        reward = self.t
+        done = False
+        return ob, reward, done, {}
+                
     def _render(self, mode='human', close=False):
-        if close:
-            if self.viewer is not None:
-                self.viewer.close()
-                self.viewer = None
-            return
-        img = self._get_image()
-        if mode == 'rgb_array':
-            return img
-        elif mode == 'human':
-            from gym.envs.classic_control import rendering
-            if self.viewer is None:
-                self.viewer = rendering.SimpleImageViewer()
-            self.viewer.imshow(img)
+        img = ImageGrab.grab(bbox =(self.x0, self.y0, self.x1, self.y1))
+        return img
+       
 
     
